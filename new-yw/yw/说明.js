9aa640ace04bb4facff9a56efc13951561ffd322
xiaogang0-0 <361204134@qq.com>
2017-04-18 11:01:08 +0800

    1

--- new-yw/yw/说明.js
+++
@@ -1,8 +0,0 @@
-1、新增快捷通道
-   学员通道   kjtd_xueyuantongdao.html
-   班级通道   kjtd_banjitongdao.html
-2、人员管理
-   教师管理   rygl_jiaoshiguanli.html
-   校区人员管理  rygl_xiaoqurenyuanguanli.html
-   c:更新文案为：'* 用户名为联系电话，初始密码为电话后四位+0000。'
-
